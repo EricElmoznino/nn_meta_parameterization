bd50c689a23e508cf1aa335ce5c8a1e58c33cd0f
EricElmoznino <eric.elmoznino@gmail.com>
2020-11-29 19:32:36 -0500

    Some argument changes

--- training/train_shoe2edge.py
+++ training/train_shoe2edge.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 from argparse import ArgumentParser
+import torch
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 from tasks import ImageTransformTask
@@ -8,6 +9,30 @@ from datasets import Shoe2EdgeDataModule
 from tasks.custom_callbacks import TensorBoardImageCallback
 from models.baselines import ShallowCNN
 from models.meta_cnns import OneLayerResNetMeta
+
+
+def main(args):
+    run_name = args.run_name if args.run_name is not None else args.model
+    save_dir = os.path.join('saved_runs', run_name)
+    if args.debug:
+        save_dir += '_DEBUG'
+        shutil.rmtree(save_dir, ignore_errors=True)
+        overfit_batches = 1
+    else:
+        overfit_batches = 0
+    os.mkdir(save_dir)
+
+    gpus = 1 if torch.cuda.is_available() else 0
+
+    model = dispatch_model(args.model)
+    data = Shoe2EdgeDataModule(data_dir=args.data_dir)
+    task = ImageTransformTask(model, **vars(args))
+
+    trainer = Trainer.from_argparse_args(args, default_root_dir=save_dir, gpus=gpus,  overfit_batches=overfit_batches,
+                                         callbacks=[ModelCheckpoint(monitor='Validation/loss'),
+                                                    EarlyStopping(monitor='Validation/loss'),
+                                                    TensorBoardImageCallback(n_cols=2)])
+    trainer.fit(task, datamodule=data)
 
 
 def dispatch_model(model_name):
@@ -20,28 +45,13 @@ def dispatch_model(model_name):
 
 
 parser = ArgumentParser()
-parser.add_argument('--run_name', type=str, default='debug')
-parser.add_argument('--data_dir', type=str, default='/home/eric/datasets/shoe2edge')
-parser.add_argument('--batch_size', type=int, default=32)
+parser.add_argument('--data_dir', type=str, required=True)
+parser.add_argument('--run_name', type=str, default=None)
 parser.add_argument('--model', type=str, default='meta')
+parser.add_argument('--debug', action='store_true')
+parser.add_argument('--batch_size', type=int, default=32)
 parser = ImageTransformTask.add_model_specific_args(parser)
 parser = Trainer.add_argparse_args(parser)
 args = parser.parse_args()
 
-save_dir = os.path.join('saved_runs', args.run_name)
-if args.run_name == 'debug':
-    shutil.rmtree(save_dir, ignore_errors=True)
-    overfit_batches = 1
-else:
-    overfit_batches = 0
-os.mkdir(save_dir)
-
-model = dispatch_model(args.model)
-data = Shoe2EdgeDataModule(data_dir=args.data_dir)
-task = ImageTransformTask(model, **vars(args))
-
-trainer = Trainer.from_argparse_args(args, default_root_dir=save_dir, overfit_batches=overfit_batches,
-                                     callbacks=[ModelCheckpoint(monitor='Validation/loss'),
-                                                EarlyStopping(monitor='Validation/loss'),
-                                                TensorBoardImageCallback(n_cols=2)])
-trainer.fit(task, datamodule=data)
+main(args)
